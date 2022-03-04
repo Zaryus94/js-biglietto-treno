8914bbf0c2cd6bb81fddff7237fc04e5b9194bb7
Christian Podda <poddachristian94@gmail.com>
2022-03-04 17:02:03 +0100

    Trying to build conditions

--- js/script.js
+++ js/script.js
@@ -1,1 +1,18 @@
-console.log('Ok!');
+// JS console check
+// console.log('JS ok!');
+
+// Prompt Km
+const Km = prompt('Benvenuto nel servizio di calcolo prezzo per i biglietti delle principali compagnie ferroviarie. Inserisci il numero di kilometri che desideri percorrere');
+if (isNaN(Km)) {
+
+}
+
+// Log Km
+console.log(Km);
+
+// Prompt Age
+const Age = prompt('Inserisci la tua età per poter usufruire dello sconto under 18 o over 65 altrimenti clicca su "Annulla"');
+
+// Log Age
+console.log(Age)
+

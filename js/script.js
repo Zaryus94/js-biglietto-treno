f392c375e4bebd3eb0f6eea7b58a6649f68ba242
Christian Podda <poddachristian94@gmail.com>
2022-03-05 13:36:10 +0100

    Ticket base price added

--- js/script.js
+++ js/script.js
@@ -1,18 +1,20 @@
 // JS console check
 // console.log('JS ok!');
 
-// Prompt Km
+// Costante del prezzo base del biglietto di viaggio
+const TicketBasePrice = 0.21;
+console.log(TicketBasePrice);
+
+// Prompt richiesta dati
 const Km = prompt('Benvenuto nel servizio di calcolo prezzo per i biglietti delle principali compagnie ferroviarie. Inserisci il numero di kilometri che desideri percorrere');
 if (isNaN(Km)) {
+    alert('Non hai inserito un numero! Ricarica la pagina e riprova');
 
+} else {
+    console.log(Km);
+    const Age = prompt('Inserisci la tua età per poter usufruire dello sconto under 18 o over 65 altrimenti clicca su "Annulla"');
+    if (isNaN(Age)) {
+        alert('Non hai inserito un numero! Ricarica la pagina e riprova');
+    }
+    else console.log(Age);
 }
-
-// Log Km
-console.log(Km);
-
-// Prompt Age
-const Age = prompt('Inserisci la tua età per poter usufruire dello sconto under 18 o over 65 altrimenti clicca su "Annulla"');
-
-// Log Age
-console.log(Age)
-
